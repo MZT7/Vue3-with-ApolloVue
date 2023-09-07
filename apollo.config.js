5f0d085ea1dedcc84bba3e8a5aa67140182e5ecc
MZT7 <40639877+MZT7@users.noreply.github.com>
2023-09-07 14:52:53 +0100

    Update apollo.config.js

--- apollo.config.js
+++ apollo.config.js
@@ -1,12 +1,12 @@
-module.exports = {
-  client: {
-    service: {
-      name: "Laravel",
-      // URL to the GraphQL API
-      // url: "http://localhost:8000/graphql",
-      url: "https://laravel-with-graphql-production.up.railway.app/graphiql",
-    },
-    // Files processed by the extension
-    includes: ["src/**/*.vue", "src/**/*.js"],
-  },
-};
+// module.exports = {
+//   client: {
+//     service: {
+//       name: "Laravel",
+//       // URL to the GraphQL API
+//       // url: "http://localhost:8000/graphql",
+//       url: "https://laravel-with-graphql-production.up.railway.app/graphiql",
+//     },
+//     // Files processed by the extension
+//     includes: ["src/**/*.vue", "src/**/*.js"],
+//   },
+// };
