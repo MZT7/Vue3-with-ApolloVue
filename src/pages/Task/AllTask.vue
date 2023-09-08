ca9a205c92ff9b1ec29e36b0d6c196ab9d232ed7
MZT7 <40639877+MZT7@users.noreply.github.com>
2023-09-08 19:42:13 +0100

    sd

--- src/pages/Task/AllTask.vue
+++ src/pages/Task/AllTask.vue
@@ -68,7 +68,7 @@ onMounted(async () => {
     // }
 
     const response = await fetch("https://laravel-with-graphql-production.up.railway.app/sanctum/csrf-cookie");
-    console.log("response", response);
+    console.log("response", response.json);
     console.log("working", 'working');
 
     const { result, loading, refetch } = useQuery(Query);
